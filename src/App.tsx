eeeba9a06c3a9ec035e024bf17b12f0635d83f6a
Carlos Martínez <carloxmz@gmail.com>
2024-05-31 17:46:11 -0600

    - Consulta del clima desde una api
    - hook personalizado para administrar el estado
    
    - Agrega variables de entorno

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import styles from "./App.module.css"
+import Form from "./components/Form/Form"
+import Spinner from "./components/Spinner/Spinner"
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
+import Alert from "./components/alert/Alert"
+import useWeather from "./components/hooks/useWeather"
+
 function App() {
+
+  const { weather, fetchWeather, hasWeatherData, loading, notFound } = useWeather()
 
   return (
     <>
-  <h1>hola</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+      <div className={styles.container}>
+        <Form
+          fetchWeather={fetchWeather}
+        />
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>Ciudad No Encontrada</Alert>}
+      </div>
     </>
   )
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,82 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { countries } from "../data/countries";
+import styles from "./Form.module.css"
+import { SearchType } from "../../types";
+import Alert from "../alert/Alert";
+
+type formProps = {
+    fetchWeather: (search: SearchType) => Promise<void>
+}
+
+export default function Form({ fetchWeather }: formProps) {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const [alert, setAlert] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (Object.values(search).includes('')) {
+            setAlert('Todos los campos son obligatorios')
+            return
+        }
+
+        fetchWeather(search)
+    }
+
+    return (
+        <form
+            className={styles.form}
+            onSubmit={handleSubmit}
+        >
+            {alert && <Alert>{alert}</Alert>}
+            <div className={styles.field}>
+                <label htmlFor="city">Ciudad:</label>
+                <input
+                    id="city"
+                    type="text"
+                    name="city"
+                    placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className={styles.field}>
+                <label htmlFor="country">País:</label>
+                <select
+                    id="country"
+                    name="country"
+                    value={search.country}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Seleccione un País -- </option>
+                    {countries.map(countrie => (
+                        <option
+                            key={countrie.code}
+                            value={countrie.code}>
+                            {countrie.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <input
+                className={styles.submit}
+                type="submit"
+                value="Consultar Clima"
+            />
+        </form>
+    )
+}
